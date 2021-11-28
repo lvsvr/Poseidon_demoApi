bf5373bd77dc40d407bf0afc91ba659ceec06ccb
w-boar <av.w.boar@gmail.com>
2021-11-28 21:37:17 +0100

    domain classes

--- demo/src/main/java/com/poseidon/api/domain/RuleName.java
+++ demo/src/main/java/com/poseidon/api/domain/RuleName.java
@@ -1,4 +1,105 @@
 package com.poseidon.api.domain;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rule_name")
 public class RuleName {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "json")
+    private String json;
+    @Column(name = "template")
+    private String template;
+    @Column(name = "sqlStr")
+    private String sqlStr;
+    @Column(name = "sqlPart")
+    private String sqlPart;
+
+    public RuleName(String name, String description, String json, String template, String sqlStr, String sqlPart) {
+        this.name = name;
+        this.description = description;
+        this.json = json;
+        this.template = template;
+        this.sqlStr = sqlStr;
+        this.sqlPart = sqlPart;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    public String getSqlStr() {
+        return sqlStr;
+    }
+
+    public void setSqlStr(String sqlStr) {
+        this.sqlStr = sqlStr;
+    }
+
+    public String getSqlPart() {
+        return sqlPart;
+    }
+
+    public void setSqlPart(String sqlPart) {
+        this.sqlPart = sqlPart;
+    }
+
+    @Override
+    public String toString() {
+        return "RuleName{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", json='" + json + '\'' +
+                ", template='" + template + '\'' +
+                ", sqlStr='" + sqlStr + '\'' +
+                ", sqlPart='" + sqlPart + '\'' +
+                '}';
+    }
+
 }
--- demo/src/main/java/com/poseidon/api/domain/User.java
+++ demo/src/main/java/com/poseidon/api/domain/User.java
@@ -1,4 +1,67 @@
 package com.poseidon.api.domain;
 
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "username")
+    @NotBlank(message = "Username is mandatory")
+    private String username;
+    @Column(name = "password")
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+    @Column(name = "fullname")
+    @NotBlank(message = "FullName is mandatory")
+    private String fullName;
+    @Column(name = "role")
+    @NotBlank(message = "Role is mandatory")
+    private String role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
